d53da0fc9d05eec8acbabb5e5ac2415b1697561e
archytechtor <58563837+archytechtor@users.noreply.github.com>
2022-08-14 14:00:37 +0500

    fix

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const config = {
   score: 0,
-	step: 0,
-	maxStep: 6,
-	sizeCell: 16,
-	sizeBerry: 6,
+  step: 0,
+  maxStep: 6,
+  sizeCell: 16,
+  sizeBerry: 6,
   snake: {
     x: 160,
     y: 160,
@@ -23,108 +23,108 @@ const scoreBlock = document.querySelector('.score .count');
 const context = canvas.getContext('2d');
 
 const scoreUp = () => {
-	config.score++;
-	drawScore();
+  config.score++;
+  drawScore();
 }
 
 const drawScore = () => {
-	scoreBlock.innerHTML = config.score;
+  scoreBlock.innerHTML = config.score;
 }
 
 const getRandom = (min, max) => {
-	return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 const randomPositionBerry = () => {
-	config.berry.x = getRandom(0, canvas.width / config.sizeCell) * config.sizeCell;
-	config.berry.y = getRandom(0, canvas.height / config.sizeCell) * config.sizeCell;
+  config.berry.x = getRandom(0, canvas.width / config.sizeCell) * config.sizeCell;
+  config.berry.y = getRandom(0, canvas.height / config.sizeCell) * config.sizeCell;
 }
 
 const drawBerry = () => {
-	context.beginPath();
-	context.fillStyle = '#f74c55';
-	context.arc(
+  context.beginPath();
+  context.fillStyle = '#f74c55';
+  context.arc(
     config.berry.x + (config.sizeCell / 2),
     config.berry.y + (config.sizeCell / 2),
     config.sizeBerry,
     0,
     2 * Math.PI
-  );
-	context.fill();
+  );  
+  context.fill();
 }
 
 const collisionBorder = () => {
-	if (config.snake.x < 0) {
-		config.snake.x = canvas.width - config.sizeCell;
-	} else if (config.snake.x >= canvas.width) {
-		config.snake.x = 0;
-	}
+  if (config.snake.x < 0) {
+    config.snake.x = canvas.width - config.sizeCell;
+  } else if (config.snake.x >= canvas.width) {
+    config.snake.x = 0;
+  }
 
-	if (config.snake.y < 0) {
-		config.snake.y = canvas.height - config.sizeCell;
-	} else if (config.snake.y >= canvas.height) {
-		config.snake.y = 0;
-	}
+  if (config.snake.y < 0) {
+    config.snake.y = canvas.height - config.sizeCell;
+  } else if (config.snake.y >= canvas.height) {
+    config.snake.y = 0;
+  }
 }
 
 const refreshGame = () => {
-	config.score = 0;
-	drawScore();
+  config.score = 0;
+  drawScore();
 
-	config.snake.x = 160;
-	config.snake.y = 160;
-	config.snake.tails = [];
-	config.snake.maxTails = 3;
-	config.snake.dx = config.sizeCell;
-	config.snake.dy = 0;
+  config.snake.x = 160;
+  config.snake.y = 160;
+  config.snake.tails = [];
+  config.snake.maxTails = 3;
+  config.snake.dx = config.sizeCell;
+  config.snake.dy = 0;
 
-	randomPositionBerry();
+  randomPositionBerry();
 }
 
 const drawSnake = () => {
-	config.snake.x += config.snake.dx;
-	config.snake.y += config.snake.dy;
+  config.snake.x += config.snake.dx;
+  config.snake.y += config.snake.dy;
 
-	collisionBorder();
+  collisionBorder();
 
-	config.snake.tails.unshift({x: config.snake.x, y: config.snake.y});
+  config.snake.tails.unshift({x: config.snake.x, y: config.snake.y});
 
-	if (config.snake.tails.length > config.snake.maxTails) {
-		config.snake.tails.pop();
-	}
+  if (config.snake.tails.length > config.snake.maxTails) {
+    config.snake.tails.pop();
+  }
 
-	config.snake.tails.forEach((el, index) => {
+  config.snake.tails.forEach((el, index) => {
     context.fillStyle = index === 0 ? '#66ca36' : '#85d55e';
-		context.fillRect(el.x, el.y, config.sizeCell, config.sizeCell);
+    context.fillRect(el.x, el.y, config.sizeCell, config.sizeCell);
 
-		if (el.x === config.berry.x && el.y === config.berry.y) {
-			config.snake.maxTails++;
-			scoreUp();
-			randomPositionBerry();
-		}
+    if (el.x === config.berry.x && el.y === config.berry.y) {
+      config.snake.maxTails++;
+      scoreUp();
+      randomPositionBerry();
+    }
 
-		for (let i = index + 1; i < config.snake.tails.length; i++) {
-			if (el.x === config.snake.tails[i].x && el.y === config.snake.tails[i].y) {
+    for (let i = index + 1; i < config.snake.tails.length; i++) {
+      if (el.x === config.snake.tails[i].x && el.y === config.snake.tails[i].y) {
         alert(`Ты набрал очков: ${config.score}`);
-				refreshGame();
-			}
-		}
-	});
+        refreshGame();
+      }
+    }
+  });
 }
 
 const gameLoop = () => {
-	requestAnimationFrame(gameLoop);
+  requestAnimationFrame(gameLoop);
 
-	if (++config.step < config.maxStep) {
-		return;
-	}
+  if (++config.step < config.maxStep) {
+    return;
+  }
 
-	config.step = 0;
+  config.step = 0;
 
-	context.clearRect(0, 0, canvas.width, canvas.height);
+  context.clearRect(0, 0, canvas.width, canvas.height);
 
-	drawBerry();
-	drawSnake();
+  drawBerry();
+  drawSnake();
 }
 
 drawScore();
@@ -135,25 +135,25 @@ document.addEventListener('keydown', (e) => {
     case 'KeyW':
     case 'ArrowUp': {
       config.snake.dy = -config.sizeCell;
-		  config.snake.dx = 0;
+      config.snake.dx = 0;
       break;
     }
     case 'KeyS':
     case 'ArrowDown': {
       config.snake.dy = config.sizeCell;
-		  config.snake.dx = 0;
+      config.snake.dx = 0;
       break;
     }
     case 'KeyA':
     case 'ArrowLeft': {
       config.snake.dx = -config.sizeCell;
-		  config.snake.dy = 0;
+      config.snake.dy = 0;
       break;
     }
     case 'KeyD':
     case 'ArrowRight': {
       config.snake.dx = config.sizeCell;
-		  config.snake.dy = 0;
+      config.snake.dy = 0;
       break;
     }
     default:
